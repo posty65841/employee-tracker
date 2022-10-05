3c83223b3ea5db834cd5d9690621a1c71732487e
austin <posty65841@gmail.com>
2022-10-04 19:53:58 -0500

    same issue as before

--- index.js
+++ index.js
@@ -86,6 +86,10 @@ async function main() {
   }
   async function addAEmployee() {
     const [rows] = await connection.execute(`SELECT * FROM employee;`)
+    inquirer
+    .prompt{
+      
+    }
 
     console.table(rows)
   }
